a1b281fa5348b98c6163212fc626c5bb4fffe4c3
Shyam Jee <shyamjee969678@gmail.com>
2025-08-03 14:01:56 +0530

    Create Search-an-element-in-an-array-linear-search.java

---
+++ Search-an-element-in-an-array-linear-search.java
@@ -0,0 +1,19 @@
+// Problem Link : https://www.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1
+
+/* Java Code */
+
+class Solution {
+
+    static int search(int arr[], int x) {
+
+        // Your code here
+        if(arr.length==0) return -1;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==x){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
