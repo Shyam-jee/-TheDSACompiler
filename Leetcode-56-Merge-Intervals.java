59cea343c9163d8d86764504b69594f938872cc8
Shyam Jee <shyamjee969678@gmail.com>
2025-08-26 13:15:42 +0530

    Create Leetcode-56-Merge-Intervals.java

---
+++ Leetcode-56-Merge-Intervals.java
@@ -0,0 +1,32 @@
+
+// Time Complexity : O(NlogN)
+// Space Complexity : O(N)
+
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)->a[0]-b[0]);
+        ArrayList<int[]> temp=new ArrayList<>();
+        for(int[] interval:intervals)
+        {
+            if(temp.isEmpty() || temp.getLast()[1]<interval[0])
+            {
+                temp.add(interval);
+            }
+            else
+            {
+                temp.getLast()[1]=Math.max(temp.getLast()[1], interval[1]);
+            }
+        }
+        int[][] res=new int[temp.size()][2];
+        for(int i=0;i<temp.size();i++)
+        {
+            int[] t=temp.get(i);
+            for(int j=0;j<2;j++)
+            {
+                res[i][j]=t[j];
+            }
+        }
+        return res;
+    }
+}
